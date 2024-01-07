7c609923ddb28bc81063c80537b6099534254335
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2024-01-07 13:03:31 +0100

    make changes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-ReactDom.render(<h1>Hello!</h1>, document.getElementById("root"))
+// const h1 = document.createElement("h1")
+// h1.textContent = "Hello world"
+// h1.className = "header"
+// console.log(h1)
+
+// JSX
+const page = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+
+console.log(page)
+
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+)
