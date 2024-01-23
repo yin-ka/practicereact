2f5197ed7fd42c38b12cd7b9adc45056808dff45
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2024-01-23 08:08:25 +0100

    Add images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,10 @@
 import React from 'react'
 import "./Navbar.css";
-import logo from "../images/reactjs-icon.png"
 
 function Navbar() {
   return (
     <>
-      <nav>
-        <div className='nav_latter'>
-          <img src={logo} alt="logo"></img>
-          <h1>ReactFacts</h1>
-        </div>
-        <div className='nav_former'>
-          <h3>React Course - Project 1</h3>
-        </div>
-      </nav>
+      <p>Navbar</p>
     </>
   )
 }
