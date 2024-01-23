f7711b39748f5481dc38c48d30a6b98a1bbb22fb
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2024-01-23 09:50:15 +0100

    set up hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Main from './components/Main';
+import Hero from './components/Hero';
 function App() {
   return (
     <>
     <Navbar />
-    <Main />
+    <Hero />
     </>
   )
 }
