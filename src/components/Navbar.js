35c8eb347cb2841f0612f750fda6aea5ff9ed1b6
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2024-01-23 08:16:48 +0100

    set up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./Navbar.css";
-
+import logo from "../images/airbnb 1.png"
 function Navbar() {
   return (
     <>
-      <p>Navbar</p>
+      <nav className='navbar'>
+        <img src={logo} className='logo' alt="logo"></img>
+      </nav>
     </>
   )
 }
