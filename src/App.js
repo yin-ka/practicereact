9ca777d6656fdd3593af453d3d897c96e177b1d2
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2024-01-26 04:27:25 +0100

    Set up new react app for new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Card from "./components/Card"
-import data from "./components/data"
 function App() {
-    const cards = data.map(item => {
-        return (
-            <Card 
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
-            />
-        )
-    })
   return (
     <>
-    <Navbar />
-    <Hero />
-    {cards}
+    
     </>
   )
 }
