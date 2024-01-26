b02f5c3d65b201e38136a876d2fcbfe38874d33a
Yusuf Sholotan <sholotanyusuf14@gmail.com>
2024-01-26 04:17:01 +0100

    Set up card components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero';
+import Hero from './components/Hero'
+import Card from "./components/Card"
+import data from "./components/data"
 function App() {
+    const cards = data.map(item => {
+        return (
+            <Card 
+                img={item.coverImg}
+                rating={item.stats.rating}
+                reviewCount={item.stats.reviewCount}
+                location={item.location}
+                title={item.title}
+                price={item.price}
+            />
+        )
+    })
   return (
     <>
     <Navbar />
     <Hero />
+    {cards}
     </>
   )
 }
 
-export default App
+export default App;
